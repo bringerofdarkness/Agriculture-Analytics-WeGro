9d93173466badc8e724a9b796fd79643dc30c7e4
Md. Shahrul Zakaria <s.zakaria.k@gmail.com>
2026-05-19 00:18:22 +0600

    fix: restore services package initializer

---
+++ app/services/__init__.py
@@ -0,0 +1,62 @@
+from app.services.crop_reports import (
+    get_crop_quality_breakdown,
+    get_crop_seasonal_trend,
+    get_crop_yield_efficiency,
+)
+from app.services.data_loader import (
+    DatabaseTable,
+    DatabaseView,
+    load_dim_crop,
+    load_dim_farm,
+    load_dim_market,
+    load_farm_profitability,
+    load_harvest_full,
+    load_revenue_by_crop_year,
+    load_source_as_dataframe,
+    load_table_as_dataframe,
+    load_view_as_dataframe,
+)
+from app.services.dataframe_utils import (
+    apply_optional_filters,
+    dataframe_to_records,
+    ensure_dataframe_not_empty,
+    normalize_filter_value,
+    round_numeric_columns,
+    validate_required_columns,
+)
+from app.services.farm_reports import (
+    get_farm_loss_analysis,
+    get_farm_summary,
+    get_single_farm_performance,
+    get_top_farms_ranking,
+)
+from app.services.market_reports import get_market_price_comparison
+
+
+__all__ = [
+    "DatabaseTable",
+    "DatabaseView",
+    "apply_optional_filters",
+    "dataframe_to_records",
+    "ensure_dataframe_not_empty",
+    "get_crop_seasonal_trend",
+    "get_crop_yield_efficiency",
+    "get_crop_quality_breakdown",
+    "get_farm_loss_analysis",
+    "get_farm_summary",
+    "get_market_price_comparison",
+    "get_single_farm_performance",
+    "get_top_farms_ranking",
+    "load_dim_crop",
+    "load_dim_farm",
+    "load_dim_market",
+    "load_farm_profitability",
+    "load_harvest_full",
+    "load_revenue_by_crop_year",
+    "load_source_as_dataframe",
+    "load_table_as_dataframe",
+    "load_view_as_dataframe",
+    "normalize_filter_value",
+    "round_numeric_columns",
+    "validate_required_columns",
+]
